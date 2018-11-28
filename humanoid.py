ee6272793e7d436c5e7da45304dcc33ee8752841
telnetr4 <getyourspyfix@gmail.com>
2018-11-28 00:07:37 -1000

    JSON Formatting
    Reformatted JSON to be more human readable.

--- humanoid.py
+++ humanoid.py
@@ -1,4 +1,4 @@
-#ManuelbastioniLAB - Copyright (C) 2015-2018 Manuel Bastioni
+material_data_prop#ManuelbastioniLAB - Copyright (C) 2015-2018 Manuel Bastioni
 #Official site: www.manuelbastioni.com
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
