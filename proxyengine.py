a56d5bc9d969e47038bc77e3dd06f5b1659ac3a6
animate1978 <rmantd.tg@gmail.com>
2018-12-16 08:54:50 -0600

    1.6.5 release

--- proxyengine.py
+++ proxyengine.py
@@ -1,17 +1,19 @@
-#ManuelbastioniLAB - Copyright (C) 2015-2018 Manuel Bastioni
-#Official site: www.manuelbastioni.com
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# ManuelbastioniLAB - Copyright (C) 2015-2018 Manuel Bastioni
+# Official site: www.manuelbastioni.com
+# MB-Lab fork website : https://github.com/animate1978/MB-Lab
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import bpy
 from . import algorithms
