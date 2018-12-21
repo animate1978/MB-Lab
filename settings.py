3b0df22106d3dab339c0c6b0f177f9940e02ab67
telnetr4 <getyourspyfix@gmail.com>
2018-12-21 09:47:28 -1000

    More compartmentalization`
    algorithims.py:
    Moved debug_level to settings.
    
    Edited print_log_report to use int level control
    
    Changed how get_data_path is obtained (checks once then stores to global variable).
    
    Made simple_path more useful (default should give path based on file's relative location to the data folder)
    
    Commented out set_modifier_parameter as it's currently unused and if it has an exception, it will fail due to undefined variable modifier_name
    
    fixed get_object_materials print_log_report so if something isn't found it doesn't break everything.
    
    settings.py:
    Moved import into init to avoid an error I kept getting. Moved mlab_ varibles into init to support this change.
    
    added debug_level and data_path global variable

--- settings.py
+++ settings.py
@@ -3,18 +3,26 @@ import os
 import json
 from bpy_extras.io_utils import ExportHelper, ImportHelper
 from bpy.app.handlers import persistent
-from . import humanoid, animationengine, proxyengine, algorithms, pose
+# from . import humanoid, animationengine, proxyengine
 # from . import bl_info
 
-mblab_humanoid = 0
-mblab_retarget = animationengine.RetargetEngine()
-mblab_shapekeys = animationengine.ExpressionEngineShapeK()
-mblab_proxy = proxyengine.ProxyEngine()
+# mblab_humanoid = 0
+# mblab_retarget = animationengine.RetargetEngine()
+# mblab_shapekeys = animationengine.ExpressionEngineShapeK()
+# mblab_proxy = proxyengine.ProxyEngine()
 
+debug_level = 0
+data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data") # from get_data_path
 
 def init(context):
+    from . import humanoid, animationengine, proxyengine
+
     global mblab_humanoid
     global mblab_retarget
     global mblab_shapekeys
     global mblab_proxy
+
     mblab_humanoid = humanoid.Humanoid(context["version"])
+    mblab_retarget = animationengine.RetargetEngine()
+    mblab_shapekeys = animationengine.ExpressionEngineShapeK()
+    mblab_proxy = proxyengine.ProxyEngine()
