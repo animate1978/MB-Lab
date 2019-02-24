a56f5dcf14b406986a662d10e6a6df9dc02e2566
animate1978 <rmantd.tg@gmail.com>
2019-02-23 22:42:59 -0600

    Added Auto updater code to init.py, need to fix

--- __init__.py
+++ __init__.py
@@ -28,13 +28,14 @@ from . import facerig
 from . import humanoid, animationengine, proxyengine
 from . import utils
 from . import algorithms
+from . import addon_updater_ops
 
 logger = logging.getLogger(__name__)
 
 bl_info = {
     "name": "MB-Lab",
     "author": "Manuel Bastioni",
-    "version": (1, 7, 2),
+    "version": (1, 7, 3),
     "blender": (2, 80, 0),
     "location": "View3D > Tools > MB-Lab",
     "description": "A complete lab for character creation",
@@ -1817,6 +1818,67 @@ class LoadTemplate(bpy.types.Operator):
             obj["manuellab_proxy_reference"] = mblab_humanoid.characters_config[scn.mblab_template_name]["template_model"]
         return {'FINISHED'}
 
+# demo bare-bones preferences
+@addon_updater_ops.make_annotations
+class DemoPreferences(bpy.types.AddonPreferences):
+	bl_idname = __package__
+
+	# addon updater preferences
+
+	auto_check_update = bpy.props.BoolProperty(
+		name="Auto-check for Update",
+		description="If enabled, auto-check for updates using an interval",
+		default=False,
+		)
+	updater_intrval_months = bpy.props.IntProperty(
+		name='Months',
+		description="Number of months between checking for updates",
+		default=0,
+		min=0
+		)
+	updater_intrval_days = bpy.props.IntProperty(
+		name='Days',
+		description="Number of days between checking for updates",
+		default=7,
+		min=0,
+		max=31
+		)
+	updater_intrval_hours = bpy.props.IntProperty(
+		name='Hours',
+		description="Number of hours between checking for updates",
+		default=0,
+		min=0,
+		max=23
+		)
+	updater_intrval_minutes = bpy.props.IntProperty(
+		name='Minutes',
+		description="Number of minutes between checking for updates",
+		default=0,
+		min=0,
+		max=59
+		)
+
+	def draw(self, context):
+		layout = self.layout
+		# col = layout.column() # works best if a column, or even just self.layout
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		# updater draw function
+		# could also pass in col as third arg
+		addon_updater_ops.update_settings_ui(self, context)
+
+		# Alternate draw function, which is more condensed and can be
+		# placed within an existing draw function. Only contains:
+		#   1) check for update/update now buttons
+		#   2) toggle for auto-check (interval will be equal to what is set above)
+		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+		# Adding another column to help show the above condensed ui as one column
+		# col = mainrow.column()
+		# col.scale_y = 2
+		# col.operator("wm.url_open","Open webpage ").url=addon_updater_ops.updater.website
+
 
 class VIEW3D_PT_tools_ManuelbastioniLAB(bpy.types.Panel):
 
@@ -2289,19 +2351,29 @@ classes = (
     CreateFaceRig,
     DeleteFaceRig,
     LoadTemplate,
+    DemoPreferences,
     VIEW3D_PT_tools_ManuelbastioniLAB,
 )
 
 
 def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
+	# addon updater code and configurations
+	# in case of broken version, try to register the updater first
+	# so that users can revert back to a working version
+	addon_updater_ops.register(bl_info)
 
+	# register the example panel, to show updater buttons
+	for cls in classes:
+		addon_updater_ops.make_annotations(cls) # to avoid blender 2.8 warnings
+		bpy.utils.register_class(cls)
 
 def unregister():
-    for cls in reversed(classes):
-        bpy.utils.unregister_class(cls)
+	# addon updater unregister
+	addon_updater_ops.unregister()
 
+	# register the example panel, to show updater buttons
+	for cls in reversed(classes):
+		bpy.utils.unregister_class(cls)
 
 if __name__ == "__main__":
     register()
