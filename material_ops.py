efeec52f94e8bb1cd0237e5f2e5815dcf1319c96
animate1978 <rmantd.tg@gmail.com>
2019-11-11 19:05:35 -0600

    Updated material_ops.py
    - Added GPL License Block

--- material_ops.py
+++ material_ops.py
@@ -1,3 +1,25 @@
+# MB-Lab
+#
+# MB-Lab fork website : https://github.com/animate1978/MB-Lab
+#
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 import logging
 import bpy
 
